53175e178e43be7534ab594df336a27c1ba3a294
verifyZeU <artemtascher-220@yandex.ru>
2024-10-01 19:30:01 +0300

    add file 4

--- 1.py
+++ 1.py
@@ -3,7 +3,7 @@ for x in range(2):
     for y in range(2):
         for z in range(2):
             for w in range(2):
-                F = ((x <= y)==(z <= w)) or (x and w)
-                if F == 0:
+                F = (x or y) <= (y == z)
+                if F == 0 :
                     print(x, y, z, w)
  
