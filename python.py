6f155e88c93e4d35bf3a2613acc47c497e69bde5
verifyZeU <artemtascher-220@yandex.ru>
2024-10-01 19:11:06 +0300

    add file 1

--- python.py
+++ python.py
@@ -3,7 +3,7 @@ for x in range(2):
     for y in range(2):
         for z in range(2):
             for w in range(2):
-                F = not(x) or not(y) or not(z) or not(w)
-                if F == 0 and x + y + z + w == 3:
+                F = (x <= y) or not(w <= z)
+                if F == 0 :
                     print(x, y, z, w)
  
